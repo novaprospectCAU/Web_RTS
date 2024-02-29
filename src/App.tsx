2e8f066275893818cba1d8c466b464339298f3e4
novaprospectCAU <novaprospect110a@gmail.com>
2024-02-29 14:04:44 +0900

    기본적인 html 레이아웃만 지정했습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,14 @@ import React from "react";
 import styles from "./App.module.css";
 
 export function App() {
-  return <div>Test</div>;
+  return (
+    <div>
+      <header className="header">this is header</header>
+      <div className="main">
+        <canvas className="mapGenerator">this is mapGenerator canvas</canvas>
+        <canvas className="inGame">this is inGame canvas</canvas>
+      </div>
+      <footer className="footer">this is footer</footer>
+    </div>
+  );
 }
