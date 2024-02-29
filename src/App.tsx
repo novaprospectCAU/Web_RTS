a4a774ba51a3623ddd445c4924715985a960a88f
novaprospectCAU <novaprospect110a@gmail.com>
2024-02-29 23:50:17 +0900

    clsx를 사용하여 리액트에 맞게 className에 해당하는 기본 태그들을 설정했습니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import React from "react";
 import styles from "./App.module.css";
+import clsx from "clsx";
 
 export function App() {
   return (
-    <div>
-      <header className="header">this is header</header>
-      <div className="main">
-        <canvas className="mapGenerator">this is mapGenerator canvas</canvas>
-        <canvas className="inGame">this is inGame canvas</canvas>
+    <div className={clsx(styles.page)}>
+      <header className={clsx(styles.header)}>this is header</header>
+      <div className={clsx(styles.main)}>
+        <canvas className={clsx(styles.mapGenerator)}>
+          this is mapGenerator canvas
+        </canvas>
+        <canvas className={clsx(styles.inGame)}>this is inGame canvas</canvas>
       </div>
-      <footer className="footer">this is footer</footer>
+      <footer className={clsx(styles.footer)}>this is footer</footer>
     </div>
   );
 }
