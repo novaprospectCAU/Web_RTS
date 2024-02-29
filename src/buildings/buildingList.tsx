ddd758ade26d07bb35afe7fcfd1db4fc5e4c2fb5
novaprospectCAU <novaprospect110a@gmail.com>
2024-02-29 16:54:36 +0900

    건물 기본 스펙을 설정했으며, 일부 속성을 조정, 추가했습니다

--- src/buildings/buildingList.tsx
+++ src/buildings/buildingList.tsx
@@ -1,7 +1,6 @@
-interface UnitFormat {
+interface BuildingFormat {
   health: number; //체력
   armor: number; //방어력
-  position: string; //배치 위치 : ground | air
   isAttackGround: boolean; //대지공격 가능 여부
   groundAttackLight: number; //경장갑대상 대지공격력
   groundAttackHeavy: number; //중장갑대상 대지공격력
@@ -14,26 +13,18 @@ interface UnitFormat {
   airAttackSpeed: number; //대공공격 속도
   airSplash: number; //대공 범위피해 (기본 : 0)
   airRange: number; //대공 사거리
-  isMovable: boolean; //이동 가능 여부 (견인포같이 고정 포대 유닛 고려)
-  movingSpeed: number; //이동 속도 (이동 가능 여부와 상관 없음)
-  price: number; //생산 비용
-  produceFrom: string; //생산 건물 (string)
-  productionTime: number; //생산 시간
-  isCollectable: boolean; //자원 채집 가능 여부
+  price: number; //건설 비용
+  buildTime: number; //건설 시간
+  isCollectable: boolean; //자원 채집(저장) 가능 여부
   sight: number; //시야 범위
-  manpower: number; //차지하는 인구수
+  isBase: boolean; //사령부 여부 확인 (광부 복귀 알고리즘용)
   targetPriority: number; //공격 대상 지정순위 (1 ~ 3(3에 가까울 수록 높은 확률))
 }
 
-/**
- * 가장 기본적인 일꾼 유닛입니다.
- * 특징 : 약한 지상 공격가능 유닛으로 자원을 채집할 수 있습니다.
- */
-export class Miner implements UnitFormat {
+class CommandCenter implements BuildingFormat {
   constructor(
     public health: number,
     public armor: number,
-    public position: string,
     public isAttackGround: boolean,
     public groundAttackLight: number,
     public groundAttackHeavy: number,
@@ -46,213 +37,170 @@ export class Miner implements UnitFormat {
     public airAttackSpeed: number,
     public airSplash: number,
     public airRange: number,
-    public isMovable: boolean,
-    public movingSpeed: number,
     public price: number,
-    public produceFrom: string,
-    public productionTime: number,
+    public buildTime: number,
     public isCollectable: boolean,
     public sight: number,
-    public manpower: number,
+    public isBase: boolean,
     public targetPriority: number
   ) {
-    this.health = 15;
-    this.armor = 0;
-    this.position = "ground";
-    this.isAttackGround = true;
-    this.groundAttackLight = 2;
-    this.groundAttackHeavy = 2;
-    this.groundAttackSpeed = 3;
-    this.groundSplash = 0;
-    this.groundRange = 1;
-    this.isAttackAir = false;
-    this.airAttackLight = 0;
-    this.airAttackHeavy = 0;
-    this.airAttackSpeed = 0;
-    this.airSplash = 0;
-    this.airRange = 0;
-    this.isMovable = true;
-    this.movingSpeed = 3;
-    this.price = 30;
-    this.produceFrom = "command";
-    this.productionTime = 12;
-    this.isCollectable = true;
-    this.sight = 10;
-    this.manpower = 1;
-    this.targetPriority = 1.8;
-  }
-}
-
-/**
- * 가장 기초적인 공격 및 정찰 유닛입니다.
- * 특징 : 약한 대지, 대공 지상 유닛으로, 싼 가격으로 가장 빨리 뽑을 수 있는 공격용 유닛입니다.
- */
-export class Soldier implements UnitFormat {
-  constructor(
-    public health: number,
-    public armor: number,
-    public position: string,
-    public isAttackGround: boolean,
-    public groundAttackLight: number,
-    public groundAttackHeavy: number,
-    public groundAttackSpeed: number,
-    public groundSplash: number,
-    public groundRange: number,
-    public isAttackAir: boolean,
-    public airAttackLight: number,
-    public airAttackHeavy: number,
-    public airAttackSpeed: number,
-    public airSplash: number,
-    public airRange: number,
-    public isMovable: boolean,
-    public movingSpeed: number,
-    public price: number,
-    public produceFrom: string,
-    public productionTime: number,
-    public isCollectable: boolean,
-    public sight: number,
-    public manpower: number,
-    public targetPriority: number
-  ) {
-    this.health = 20;
-    this.armor = 0;
-    this.position = "ground";
-    this.isAttackGround = true;
-    this.groundAttackLight = 4;
-    this.groundAttackHeavy = 4;
-    this.groundAttackSpeed = 5;
-    this.groundSplash = 0;
-    this.groundRange = 4;
-    this.isAttackAir = true;
-    this.airAttackLight = 4;
-    this.airAttackHeavy = 4;
-    this.airAttackSpeed = 5;
-    this.airSplash = 0;
-    this.airRange = 4;
-    this.isMovable = true;
-    this.movingSpeed = 3;
-    this.price = 30;
-    this.produceFrom = "barrack";
-    this.productionTime = 10;
-    this.isCollectable = false;
-    this.sight = 10;
-    this.manpower = 1;
-    this.targetPriority = 2.5;
-  }
-}
-
-/**
- * 중장갑 만능 대지 유닛입니다.
- * 특징 : 강한 대지 유닛으로 대공을 제외한 대부분의 역할을 맡을 수 있습니다.
- */
-export class MediumTank implements UnitFormat {
-  constructor(
-    public health: number,
-    public armor: number,
-    public position: string,
-    public isAttackGround: boolean,
-    public groundAttackLight: number,
-    public groundAttackHeavy: number,
-    public groundAttackSpeed: number,
-    public groundSplash: number,
-    public groundRange: number,
-    public isAttackAir: boolean,
-    public airAttackLight: number,
-    public airAttackHeavy: number,
-    public airAttackSpeed: number,
-    public airSplash: number,
-    public airRange: number,
-    public isMovable: boolean,
-    public movingSpeed: number,
-    public price: number,
-    public produceFrom: string,
-    public productionTime: number,
-    public isCollectable: boolean,
-    public sight: number,
-    public manpower: number,
-    public targetPriority: number
-  ) {
-    this.health = 45;
-    this.armor = 2;
-    this.position = "ground";
-    this.isAttackGround = true;
-    this.groundAttackLight = 15;
-    this.groundAttackHeavy = 15;
-    this.groundAttackSpeed = 3;
-    this.groundSplash = 1;
-    this.groundRange = 6;
-    this.isAttackAir = false;
-    this.airAttackLight = 0;
-    this.airAttackHeavy = 0;
-    this.airAttackSpeed = 0;
-    this.airSplash = 0;
-    this.airRange = 0;
-    this.isMovable = true;
-    this.movingSpeed = 3;
-    this.price = 100;
-    this.produceFrom = "factory";
-    this.productionTime = 35;
-    this.isCollectable = false;
-    this.sight = 10;
-    this.manpower = 3;
-    this.targetPriority = 2.5;
-  }
-}
-
-/**
- * 중장갑 공중 정찰 및 대지 공격 유닛입니다.
- * 특징 : 강하지 않지만 기동성이 좋은 유닛으로 초중반에 주로 유용한 유닛입니다.
- */
-export class ReconHeli implements UnitFormat {
-  constructor(
-    public health: number,
-    public armor: number,
-    public position: string,
-    public isAttackGround: boolean,
-    public groundAttackLight: number,
-    public groundAttackHeavy: number,
-    public groundAttackSpeed: number,
-    public groundSplash: number,
-    public groundRange: number,
-    public isAttackAir: boolean,
-    public airAttackLight: number,
-    public airAttackHeavy: number,
-    public airAttackSpeed: number,
-    public airSplash: number,
-    public airRange: number,
-    public isMovable: boolean,
-    public movingSpeed: number,
-    public price: number,
-    public produceFrom: string,
-    public productionTime: number,
-    public isCollectable: boolean,
-    public sight: number,
-    public manpower: number,
-    public targetPriority: number
-  ) {
-    this.health = 40;
+    this.health = 500;
     this.armor = 1;
-    this.position = "air";
-    this.isAttackGround = true;
-    this.groundAttackLight = 8;
-    this.groundAttackHeavy = 8;
-    this.groundAttackSpeed = 2;
+    this.isAttackGround = false;
+    this.groundAttackLight = 0;
+    this.groundAttackHeavy = 0;
+    this.groundAttackSpeed = 0;
     this.groundSplash = 0;
-    this.groundRange = 4;
+    this.groundRange = 0;
     this.isAttackAir = false;
     this.airAttackLight = 0;
     this.airAttackHeavy = 0;
     this.airAttackSpeed = 0;
     this.airSplash = 0;
     this.airRange = 0;
-    this.isMovable = true;
-    this.movingSpeed = 3.5;
-    this.price = 60;
-    this.produceFrom = "factory";
-    this.productionTime = 32;
+    this.price = 250;
+    this.buildTime = 80;
+    this.isCollectable = true;
+    this.sight = 12;
+    this.isBase = true;
+    this.targetPriority = 1.0;
+  }
+}
+
+class Barrack implements BuildingFormat {
+  constructor(
+    public health: number,
+    public armor: number,
+    public isAttackGround: boolean,
+    public groundAttackLight: number,
+    public groundAttackHeavy: number,
+    public groundAttackSpeed: number,
+    public groundSplash: number,
+    public groundRange: number,
+    public isAttackAir: boolean,
+    public airAttackLight: number,
+    public airAttackHeavy: number,
+    public airAttackSpeed: number,
+    public airSplash: number,
+    public airRange: number,
+    public price: number,
+    public buildTime: number,
+    public isCollectable: boolean,
+    public sight: number,
+    public isBase: boolean,
+    public targetPriority: number
+  ) {
+    this.health = 400;
+    this.armor = 1;
+    this.isAttackGround = false;
+    this.groundAttackLight = 0;
+    this.groundAttackHeavy = 0;
+    this.groundAttackSpeed = 0;
+    this.groundSplash = 0;
+    this.groundRange = 0;
+    this.isAttackAir = false;
+    this.airAttackLight = 0;
+    this.airAttackHeavy = 0;
+    this.airAttackSpeed = 0;
+    this.airSplash = 0;
+    this.airRange = 0;
+    this.price = 100;
+    this.buildTime = 50;
     this.isCollectable = false;
-    this.sight = 10;
-    this.manpower = 2;
-    this.targetPriority = 2.2;
+    this.sight = 12;
+    this.isBase = false;
+    this.targetPriority = 1.2;
+  }
+}
+
+class Factory implements BuildingFormat {
+  constructor(
+    public health: number,
+    public armor: number,
+    public isAttackGround: boolean,
+    public groundAttackLight: number,
+    public groundAttackHeavy: number,
+    public groundAttackSpeed: number,
+    public groundSplash: number,
+    public groundRange: number,
+    public isAttackAir: boolean,
+    public airAttackLight: number,
+    public airAttackHeavy: number,
+    public airAttackSpeed: number,
+    public airSplash: number,
+    public airRange: number,
+    public price: number,
+    public buildTime: number,
+    public isCollectable: boolean,
+    public sight: number,
+    public isBase: boolean,
+    public targetPriority: number
+  ) {
+    this.health = 450;
+    this.armor = 2;
+    this.isAttackGround = false;
+    this.groundAttackLight = 0;
+    this.groundAttackHeavy = 0;
+    this.groundAttackSpeed = 0;
+    this.groundSplash = 0;
+    this.groundRange = 0;
+    this.isAttackAir = false;
+    this.airAttackLight = 0;
+    this.airAttackHeavy = 0;
+    this.airAttackSpeed = 0;
+    this.airSplash = 0;
+    this.airRange = 0;
+    this.price = 200;
+    this.buildTime = 60;
+    this.isCollectable = false;
+    this.sight = 12;
+    this.isBase = false;
+    this.targetPriority = 1.2;
+  }
+}
+
+class SupplyDepot implements BuildingFormat {
+  constructor(
+    public health: number,
+    public armor: number,
+    public isAttackGround: boolean,
+    public groundAttackLight: number,
+    public groundAttackHeavy: number,
+    public groundAttackSpeed: number,
+    public groundSplash: number,
+    public groundRange: number,
+    public isAttackAir: boolean,
+    public airAttackLight: number,
+    public airAttackHeavy: number,
+    public airAttackSpeed: number,
+    public airSplash: number,
+    public airRange: number,
+    public price: number,
+    public buildTime: number,
+    public isCollectable: boolean,
+    public sight: number,
+    public isBase: boolean,
+    public targetPriority: number
+  ) {
+    this.health = 200;
+    this.armor = 1;
+    this.isAttackGround = false;
+    this.groundAttackLight = 0;
+    this.groundAttackHeavy = 0;
+    this.groundAttackSpeed = 0;
+    this.groundSplash = 0;
+    this.groundRange = 0;
+    this.isAttackAir = false;
+    this.airAttackLight = 0;
+    this.airAttackHeavy = 0;
+    this.airAttackSpeed = 0;
+    this.airSplash = 0;
+    this.airRange = 0;
+    this.price = 60;
+    this.buildTime = 30;
+    this.isCollectable = false;
+    this.sight = 12;
+    this.isBase = false;
+    this.targetPriority = 1.0;
   }
 }
